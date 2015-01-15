59227ec752b7c5a43443b53ef488e7429a9d22fb
mattkiernan <matt.kiernan@gmail.com>
2015-01-15 09:46:29 -0500

    Drop teams table

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115142155) do
+ActiveRecord::Schema.define(version: 20150115144242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "teams", force: true do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
